8decda8da5f788fa85c3a3910a237d7a6f9f86f5
alexwing <alexwing@gmail.com>
2015-07-27 20:51:48 +0200

    deshacer de nuevo todos los cambions me quedo como estaba

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,21 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/author', function(req, res) {
-  res.render('author');
-});
+var quizController = require('../controllers/quiz_controller');
+var authorController = require('../controllers/author_controller');
 
-
-/* GET home page. */
+// Home page
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Bienvenido al juego de preguntas!' });
+  res.render('index', { title: 'Quiz' });
 });
+
+// Questions page
+router.get('/quizes/question', quizController.question);
+
+// Answers page
+router.get('/quizes/answer', quizController.answer);
+
+// Author page
+router.get('/author', authorController.author);
 
 module.exports = router;
